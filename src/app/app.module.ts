6482bb5c20b66d33a12626fa7d582c8cac85622b
Gabriela Fernanda Abbate <18758@proway.treina>
2021-02-18 19:03:19 -0300

    criando os componentes de menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { MenuComponent } from './menu/menu.component';
-import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './navegacao/footer/footer.component';
+import { MenuComponent } from './navegacao/menu/menu.component';
+import { HomeComponent } from './navegacao/home/home.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     MenuComponent,
-    HomeComponent
+    HomeComponent,
+    SobreComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/institucional/sobre/sobre.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contato',
+  templateUrl:'/sobre.component.html',
+  styles: [
+  ]
+})
+export class SobreComponent {
+
+  constructor() { }
+
+}
