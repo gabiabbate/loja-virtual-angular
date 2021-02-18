782cc1833d853aad34a1401dc430d4c2119d61d9
Gabriela Fernanda Abbate <18758@PROWAY.TREINA>
2021-02-18 19:45:29 -0300

    criando sistema basico de roteamento

---
+++ src/app/app.route.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { HomeComponent} from './navegacao/home/home.component';
+
+
+export const rooteRouterConfig: Routes = [
+    {path: '', redirectTo: '/home', pathMatch:'full'},
+    {path: 'home', component: HomeComponent },
+];
