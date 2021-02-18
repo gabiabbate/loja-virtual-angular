6d5308727a760ba7c8d2c15144dd782634c9a6a2
Gabriela Fernanda Abbate <18758@PROWAY.TREINA>
2021-02-17 21:42:49 -0300

    reestruturando app - rodape

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,17 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl:'./footer.component.html',
   styles: [
-
   ]
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
