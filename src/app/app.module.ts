ac0a4a786ef26fff67c7ffab90596230fd45168b
Gabriela Fernanda Abbate <18758@PROWAY.TREINA>
2021-02-17 21:56:36 -0300

    reestruturando app - menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl:'./menu.component.html',
+  styles: [
+  ]
+})
+export class FooterComponent {
+
+  constructor() { }
+
+}
+
